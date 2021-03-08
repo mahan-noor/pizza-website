f66de2cd4faaa71b06eee3207f17ebeccd4ee221
mahan-noor <ismahanabey@gmail.com>
2021-03-08 11:15:03 +0300

    add script.js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    //form section to submit the users input calculate the total for the order//
+    $(text-center).submit(function(event){
+        //function to get users input from the form
+        function flavour(){
+            var pizzaFlavour = document.getElementById("flavour").value;
+            return parseInt(pizzaFlavour)
+
+        }
+        function size(){
+            var pizzaSize = document.getElementById("size").value;
+            return parseInt(pizzaSize)
+
+        }
+        function crust(){
+            var pizzaCrust = document.getElementById("crust").value;
+            return parseInt(pizzaCrust)
+
+        }
+        function toppings(){
+            var pizzaToppings = document.getElementById("toppings").value;
+            return parseInt(pizzaToppings)
+
+        }
+        function number(){
+            var pizzaNumber = document.getElementById("number").value;
+            return parseInt(pizzaNumber)
+
+        }
+        //a constructor to create objects
+        function order(flavour, size, crust, toppings, quantity){
+            this.newFlavour
+        }
+    })
+})
